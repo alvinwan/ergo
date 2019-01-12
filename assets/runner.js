98d9e7e20927e789dfa90885d99254b10c906475
Alvin Wan <alvinwan1@gmail.com>
2019-01-13 00:51:12 +0800

    cleanup js

--- assets/runner.js
+++ assets/runner.js
@@ -2,36 +2,44 @@
  * Created by Alvin Wan (alvinwan.com)
  **/
 
-/********************
- * DESKTOP CONTROLS *
- ********************/
+const POSITION_X_LEFT = -0.5;
+const POSITION_X_CENTER = 0;
+const POSITION_X_RIGHT = 0.5;
 
-const PLAYER_POSITION_LEFT = {x: -0.5, y: 0, z: 0}
-const PLAYER_POSITION_CENTER = {x: 0, y: 0, z: 0}
-const PLAYER_POSITION_RIGHT = {x: 0.5, y: 0, z: 0}
+/************
+ * CONTROLS *
+ ************/
 
-// maps 0 to left, 1 to center, 2 to right
-const PLAYER_POSITION_MAPPING = [
-  PLAYER_POSITION_LEFT,
-  PLAYER_POSITION_CENTER,
-  PLAYER_POSITION_RIGHT
-]
+// Position is one of 0 (left), 1 (center), or 2 (right)
+var player_position_index = 1;
 
-var player_position_index = 1;  // start at 1 for center
+/**
+ * Move player to provided index
+ * @param {int} Lane to move player to
+ */
+function movePlayerTo(position_index) {
+  if (position_index < 0) position_index = 0;
+  if (position_index > 2) position_index = 2;
+  player_position_index = position_index;
 
-function movePlayerTo(to) {
-  if (to > 2) to = 2;
-  if (to < 0) to = 0;
-
-  player_position_index = to;
-  position = PLAYER_POSITION_MAPPING[player_position_index];
-  document.getElementById('player-container').setAttribute('position', position);
+  position = {x: 0, y: 0, z: 0}
+  if      (player_position_index == 0) position.x = POSITION_X_LEFT;
+  else if (player_position_index == 1) position.x = POSITION_X_CENTER;
+  else                                 position.x = POSITION_X_RIGHT;
+  document.getElementById('player').setAttribute('position', position);
 }
 
+/**
+ * Determine how `movePlayerTo` will be fired.
+ * If desktop, use arrow or WASD keys. If mobile, use camera's rotation.
+ **/
 function setupControls() {
-  window.onkeydown = function(e) {
-    if (!mobileAndTabletcheck() && maybeStartGame()) return;
+  return mobileCheck() ? setupMobileControls() : setupDesktopControls();
+}
 
+function setupDesktopControls() {
+  window.onkeydown = function(e) {
+    startGame();
     switch (e.keyCode) {
       case 37:  // left
       case 65:  // a
@@ -41,63 +49,23 @@ function setupControls() {
       case 68:  // d
         movePlayerTo(player_position_index + 1)
         break;
-      case 38:  // up
-      case 87:  // w
-        break;
-      case 40:  // down
-      case 83:  // s
-        break;
       default:
         break;
     }
   }
 }
 
-/*******************
- * MOBILE CONTROLS *
- *******************/
-
-var startGameTimer = null;
-var startGameIn;
-
-function setupStartGameTimer() {
-  document.getElementById('game-start-timer').setAttribute('value', '');
-}
-
-function runStartGameTimer() {
-  startGameTimer = setInterval(updateStartGameTimer, 1000);
-
-  startGameIn = 11;
-  updateStartGameTimer();
-}
-
-function updateStartGameTimer() {
-  startGameIn -= 1;
-  document.getElementById('game-start-timer').setAttribute('value', startGameIn);
-
-  if (startGameIn == 0) {
-    startGame();
-    teardownStartGameTimer();
-  }
-}
-
-function teardownStartGameTimer() {
-  clearInterval(startGameTimer);
-  startGameTimer = null;
-  document.getElementById('game-start-timer').setAttribute('value', '');
-}
-
-AFRAME.registerComponent('lane-controls', {
-  tick: function () {
-    if (mobileAndTabletcheck()) {
+function setupMobileControls() {
+  AFRAME.registerComponent('lane-controls', {
+    tick: function () {
       var rotation = this.el.object3D.rotation;
 
-      if (rotation.y > 0.1) movePlayerTo(0);
+      if      (rotation.y > 0.1)  movePlayerTo(0);
       else if (rotation.y < -0.1) movePlayerTo(2);
-      else movePlayerTo(1);
+      else                        movePlayerTo(1);
     }
-  }
-})
+  })
+}
 
 /*********
  * TREES *
@@ -111,18 +79,14 @@ var numberOfTrees = 0;
 var treeTimer;
 
 function setupTrees() {
-  templateTreeLeft = document.getElementById('template-tree-left');
-  templateTreeCenter = document.getElementById('template-tree-center');
-  templateTreeRight = document.getElementById('template-tree-right');
-  treeContainer = document.getElementById('tree-container');
+  templateTreeLeft    = document.getElementById('template-tree-left');
+  templateTreeCenter  = document.getElementById('template-tree-center');
+  templateTreeRight   = document.getElementById('template-tree-right');
+  treeContainer       = document.getElementById('tree-container');
 
   removeTree(templateTreeLeft);
   removeTree(templateTreeRight);
   removeTree(templateTreeCenter);
-
-  templateTreeLeft = templateTreeLeft.cloneNode(true);
-  templateTreeCenter = templateTreeCenter.cloneNode(true);
-  templateTreeRight = templateTreeRight.cloneNode(true);
 }
 
 function teardownTrees() {
@@ -131,8 +95,7 @@ function teardownTrees() {
 
 function addTree(el) {
   numberOfTrees += 1;
-  el.id = 'tree-container-' + numberOfTrees;
-  el.children[0].id = 'tree-' + numberOfTrees;
+  el.id = 'tree-' + numberOfTrees;
   treeContainer.appendChild(el);
 }
 
@@ -140,39 +103,36 @@ function removeTree(tree) {
   tree.parentNode.removeChild(tree);
 }
 
-function addTreesRandomly(config) {
-
-  config = config || {}
-  probTreeLeft = config['probTreeLeft'] || 0.5;
-  probTreeRight = config['probTreeRight'] || 0.5;
-  probTreeCenter = config['probTreeCenter'] || 0.5;
-  maxNumberTrees = config['maxNumberTrees'] || 2;
-
-  numberOfTreesAdded = 0;
+/**
+ * Add any number of trees across different lanes, randomly.
+ **/
+function addTreesRandomly(
+  {
+    probTreeLeft = 0.5,
+    probTreeCenter = 0.5,
+    probTreeRight = 0.5,
+    maxNumberTrees = 2
+  } = {}) {
 
   var trees = [
-    {probability: probTreeLeft, template: templateTreeLeft},
+    {probability: probTreeLeft,   template: templateTreeLeft},
     {probability: probTreeCenter, template: templateTreeCenter},
-    {probability: probTreeRight, template: templateTreeRight},
+    {probability: probTreeRight,  template: templateTreeRight},
   ]
   shuffle(trees);
 
-  for (i = 0; i < trees.length; i++) {
-    tree = trees[i];
+  var numberOfTreesAdded = 0;
+  trees.forEach(function (tree) {
     if (Math.random() < tree.probability && numberOfTreesAdded < maxNumberTrees) {
       addTree(tree.template.cloneNode(true));
       numberOfTreesAdded += 1;
     }
-  }
+  });
 
   return numberOfTreesAdded;
 }
 
-function loopAddTreesRandomly(config) {
-  config = config || {};
-  intervalLength = config['intervalLength'] || 500;
-
-  console.log('Starting to loop trees...')
+function addTreesRandomlyLoop({intervalLength = 500} = {}) {
   treeTimer = setInterval(addTreesRandomly, intervalLength);
 }
 
@@ -180,24 +140,29 @@ function loopAddTreesRandomly(config) {
  * COLLISIONS *
  **************/
 
+const POSITION_Z_OUT_OF_SIGHT = 1;
+const POSITION_Z_LINE_START = 0.6;
+const POSITION_Z_LINE_END = 0.7;
+
 AFRAME.registerComponent('player', {
   tick: function() {
-    document.querySelectorAll('.tree .movable').forEach(function(tree) {
+    document.querySelectorAll('.tree').forEach(function(tree) {
       position = tree.getAttribute('position');
-      tree_index = tree.getAttribute('data-tree-index');
+      tree_position_index = tree.getAttribute('data-tree-position-index');
       tree_id = tree.getAttribute('id');
 
-      if (position.z > 4.5) {
+      if (position.z > POSITION_Z_OUT_OF_SIGHT) {
         removeTree(tree);
       }
 
       if (!isGameRunning) return;
 
-      if (1.8 < position.z && position.z < 2.2 && tree_index == player_position_index) {
+      if (POSITION_Z_LINE_START < position.z && position.z < POSITION_Z_LINE_END
+          && tree_position_index == player_position_index) {
         gameOver();
       }
 
-      if (position.z > 2.6 && !countedTrees.has(tree_id)) {
+      if (position.z > POSITION_Z_LINE_END && !countedTrees.has(tree_id)) {
         addScoreForTree(tree_id);
         updateScoreDisplay();
       }
@@ -255,10 +220,10 @@ function showEntity(el) {
 }
 
 function setupAllMenus() {
-  menuStart = document.getElementById('start-menu');
-  menuGameOver = document.getElementById('game-over');
+  menuStart     = document.getElementById('start-menu');
+  menuGameOver  = document.getElementById('game-over');
   menuContainer = document.getElementById('menu-container');
-  startButton = document.getElementById('start-button');
+  startButton   = document.getElementById('start-button');
   restartButton = document.getElementById('restart-button');
 
   showStartMenu();
@@ -294,51 +259,41 @@ function showStartMenu() {
   restartButton.classList.remove('clickable');
 }
 
+/******
+ * UI *
+ ******/
+
+function setupCursor() {
+  if (!mobileCheck()) {
+    document.getElementById('cursor-mobile').setAttribute('visible', false);
+  }
+}
+
 /********
  * GAME *
  ********/
 
 function gameOver() {
   isGameRunning = false;
+  
   showGameOverMenu();
   setupInstructions();
   teardownTrees();
   teardownScore();
 }
 
-function maybeStartGame() {
-  if (!isGameRunning) {
-    startGame();
-    return true;
-  }
-  return false;
-}
-
 function startGame() {
   if (isGameRunning) return;
-
   isGameRunning = true;
-  hideAllMenus();
-  loopAddTreesRandomly();
 
+  hideAllMenus();
   setupScore();
   updateScoreDisplay();
-}
-
-function instructionsDisplay(cls, display) {
-  document.querySelectorAll(cls).forEach(function (el) {
-    el.style.display = display
-  });
-}
-
-function setupCursor() {
-  if (!mobileAndTabletcheck()) {
-    document.getElementById('cursor-mobile').setAttribute('visible', false);
-  }
+  addTreesRandomlyLoop();
 }
 
 function setupInstructions() {
-  if (mobileAndTabletcheck()) {
+  if (mobileCheck()) {
     hideEntity(document.getElementById('start-copy-desktop'));
     hideEntity(document.getElementById('game-over-copy-desktop'));
     showEntity(document.getElementById('start-copy-mobile'));
@@ -352,7 +307,6 @@ function setupInstructions() {
 }
 
 window.onload = function() {
-  setupStartGameTimer();
   setupAllMenus();
   setupControls();
   setupTrees();
@@ -365,9 +319,9 @@ window.onload = function() {
  *************/
 
 /**
-* Shuffles array in place.
-* @param {Array} a items An array containing the items.
-*/
+ * Shuffles array in place.
+ * @param {Array} a items An array containing the items.
+ */
 function shuffle(a) {
    var j, x, i;
    for (i = a.length - 1; i > 0; i--) {
@@ -379,7 +333,10 @@ function shuffle(a) {
    return a;
 }
 
-function mobileAndTabletcheck() {
+/**
+ * Checks for mobile and tablet platforms.
+ */
+function mobileCheck() {
   var check = false;
   (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
   return check;
